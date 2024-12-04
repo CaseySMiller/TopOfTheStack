44ae59cb311245294e28d86cc12d90a908a52922
CaseySMiller <CaseySMiller@yahoo.com>
2024-12-04 07:35:41 -0700

    added profile image to navbar

--- client/src/components/topNav.tsx
+++ client/src/components/topNav.tsx
@@ -1,18 +1,14 @@
 import { Link } from 'react-router-dom'
 // import { useState, useEffect } from 'react';
 // import Auth from '../utils/auth'
-// import { Avatar } from '@radix-ui/react-avatar';
 
 export default function TopNav({ profile, setProfile } : { profile: any, setProfile: any }) {
-  
-  console.log(profile);
-  console.log(setProfile);
   
 
   return (
     <nav className='mt-0 py-2 px-4 w-full bg-gray-800 text-white flex justify-between items-center'>
       <h1 className='text-4xl font-bold text-center'>
-        Hello {profile.username ? profile.username : 'Unknown User'}!
+        Top of the Stack
       </h1>
       <div className='flex items-center justify-center'>
         <Link to="/login">
@@ -25,9 +21,11 @@ export default function TopNav({ profile, setProfile } : { profile: any, setProf
           <button className='ml-4 w-32 text-center'>Dashboard</button>
         </Link>
       </div>
-      {/* <div>
-        <Avatar>{profile?.data?.username}</Avatar>
-      </div> */}
+      <div className='bg-gray-600 w-10 h-10 rounded-full flex place-content-center'>
+        <span className='text-2xl font-bold mt-1'>
+            {profile?.username ? profile.username.slice(0,1).toUpperCase() : '?'}
+        </span>
+      </div>
     </nav>
   )
 }
